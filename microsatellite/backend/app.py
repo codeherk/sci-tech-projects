a8ae14bf300500fb44ce675dc12f1eab503bdaf4
Byron Jenkins <codeherk@gmail.com>
2023-04-25 15:37:30 -0400

    Add error handling * Fix connection issue

--- microsatellite/backend/app.py
+++ microsatellite/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 import mysql.connector
 import os
@@ -14,48 +14,56 @@ user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DATABASE")
 
-# Set up a MySQL connection
-db = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
-
 # Define a route for the microsatellite API
 @app.route('/microsatellites')
 def get_microsatellites():
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
+    try:
+        # Set up a MySQL connection
+        conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
 
-    # Execute a SELECT query to retrieve all microsatellites from the database
-    cursor.execute("SELECT * FROM microsatellites")
+        # Create a cursor object to execute SQL queries
+        cursor = conn.cursor()
 
-    # Fetch all rows as a list of tuples
-    rows = cursor.fetchall()
+        # Execute a SELECT query to retrieve all microsatellites from the database
+        cursor.execute("SELECT * FROM microsatellites")
 
-    # Create an empty list to store the microsatellites as dictionaries
-    microsatellites = []
+        # Fetch all rows as a list of tuples
+        rows = cursor.fetchall()
 
-    # For each row in the results
-    for row in rows:
-        # Create a dictionary for the microsatellite data
-        microsatellite = {
-            "id": row[0],
-            "name": row[1],
-            "base": row[2],
-            "repeats": row[3]
-        }
+        # Create an empty list to store the microsatellites as dictionaries
+        microsatellites = []
 
-        # Add the dictionary to the list of microsatellites
-        microsatellites.append(microsatellite)
+        # For each row in the results
+        for row in rows:
+            # Create a dictionary for the microsatellite data
+            microsatellite = {
+                "id": row[0],
+                "name": row[1],
+                "base": row[2],
+                "repeats": row[3]
+            }
 
-    # Close the cursor
-    cursor.close()
-    # db.close()
+            # Add the dictionary to the list of microsatellites
+            microsatellites.append(microsatellite)
 
-    # Return the list of microsatellites as JSON
-    return jsonify(microsatellites)
+        cursor.close() 
+        # Return the list of microsatellites as JSON
+        response = jsonify(microsatellites)
+        response.status_code = 200
+        return response
+
+    except Exception as err:
+        print(err)
+        message = jsonify(message='Internal Server Error')
+        return make_response(message, 500)
+    finally: 
+        conn.close()  
+
 
 # Run the Flask app
 if __name__ == '__main__':
