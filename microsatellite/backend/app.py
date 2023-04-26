4e4ff8182b32b7c7128a35103e6656dabda51d29
Byron Jenkins <codeherk@gmail.com>
2023-04-25 20:47:58 -0400

    Improve request error handling * Update logging

--- microsatellite/backend/app.py
+++ microsatellite/backend/app.py
@@ -4,20 +4,25 @@ import mysql.connector
 import os
 import logging
 
-select_microsatellites_statement = "SELECT * FROM microsatellites"
-insert_microsatellites_statement = ("INSERT INTO microsatellites "
-               "(name, base, repeats) "
-               "VALUES (%s, %s, %s)")
+select_microsatellites_statement = 'SELECT * FROM microsatellites'
+insert_microsatellites_statement = ('INSERT INTO microsatellites '
+               '(name, base, repeats) '
+               'VALUES (%s, %s, %s)')
 
 # Create a Flask app
 app = Flask(__name__)
 CORS(app)
 
+logger = logging.getLogger('werkzeug')
+logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler('app.log')
+# logger.addHandler(handler)
+
 # Get environment variables
-host = os.getenv("MYSQL_HOST")
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-database = os.getenv("MYSQL_DATABASE")
+host = os.getenv('MYSQL_HOST')
+user = os.getenv('MYSQL_USER')
+password = os.getenv('MYSQL_PASSWORD')
+database = os.getenv('MYSQL_DATABASE')
 
 # Define a route for the microsatellite API
 @app.route('/microsatellites', methods = ['GET'])
@@ -33,8 +38,7 @@ def get_microsatellites():
 
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
-
-        logging.info("Fetching data from Microsatellites table")
+        logger.info('Fetching data from Microsatellites table.')
 
         # Execute a SELECT query to retrieve all microsatellites from the database
         cursor.execute(select_microsatellites_statement)
@@ -49,30 +53,28 @@ def get_microsatellites():
         for row in rows:
             # Create a dictionary for the microsatellite data
             microsatellite = {
-                "id": row[0],
-                "name": row[1],
-                "base": row[2],
-                "repeats": row[3]
+                'id': row[0],
+                'name': row[1],
+                'base': row[2],
+                'repeats': row[3]
             }
 
             # Add the dictionary to the list of microsatellites
             microsatellites.append(microsatellite)
 
         cursor.close()
+        conn.close()  
 
-        logging.info("Fetch successful")
+        logger.info('Fetch successful.')
 
         # Return the list of microsatellites as JSON
         response = jsonify(microsatellites)
         response.status_code = 200
         return response
-
     except Exception as err:
         print(err)
         message = jsonify(message='Internal Server Error')
         return make_response(message, 500)
-    finally: 
-        conn.close()  
 
 
 @app.route('/microsatellites', methods = ['POST'])
@@ -80,14 +82,14 @@ def post_microsatellites():
     try:
         data = request.get_json()
 
-        name = data['name']
-        base = data['base']
-        repeats = data['repeats']
+        name = data.get('name', '')
+        base = data.get('base', '')
+        repeats = data.get('repeats', '')
 
-        if name is '' or repeats is '' or name is '':
+        if name == '' or repeats == '' or name == '':
+            logger.info('Invalid request. All values for microsatellite were not provided.')
             message = jsonify(message='Bad Request')
             return make_response(message, 400)
-
         # Set up a MySQL connection
         conn = mysql.connector.connect(
             host=host,
@@ -99,7 +101,7 @@ def post_microsatellites():
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
 
-        logging.info("Inserting data into Microsatellites table")
+        logger.info('Inserting data into Microsatellites table.')
 
         # Execute a SELECT query to retrieve all microsatellites from the database
         cursor.execute(insert_microsatellites_statement, (name,base,repeats))
@@ -108,20 +110,23 @@ def post_microsatellites():
         conn.commit()
         
         cursor.close()
+        conn.close()
 
-        logging.info("Insertion successful")
+        logger.info('Insertion successful.')
         # Return success
         return ('', 200)
 
     except Exception as err:
         print(err)
-        message = jsonify(message='Internal Server Error')
-        return make_response(message, 500)
-    finally: 
-        conn.close()  
+        message = jsonify(message='Internal Server Error.')
+        return make_response(message, 500) 
 
+@app.before_request
+def log_request_info():
+    logger.debug('Headers: %s', request.headers)
+    logger.debug('Body: %s', request.get_data())
 
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
-    logging.info("Now running Microsatllites API")
+    logger.info('Now running Microsatellites API.')
