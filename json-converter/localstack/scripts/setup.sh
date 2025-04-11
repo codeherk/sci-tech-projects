e659aad0f8cca42b6d6694c22557fab8d8237464
Byron Jenkins <codeherk@gmail.com>
2025-04-11 19:44:20 -0400

    Add localstack configuration, update Makefile, and implement logging in main.py

--- json-converter/localstack/scripts/setup.sh
+++ json-converter/localstack/scripts/setup.sh
@@ -4,10 +4,14 @@ echo "Creating localstack S3 bucket..."
 awslocal --endpoint-url=http://localhost:4566 s3 mb s3://sci-tech-scholars-bucket
 
 echo "Creating localstack lambda..."
+echo "VARS: ${DEBUG} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} ${PYDEVD_DISABLE_FILE_VALIDATION}"
 # Create a lambda with mounted zip file
 awslocal --endpoint-url=http://localhost:4566 lambda create-function \
 --function-name json-converter \
 --zip-file fileb:///var/lib/localstack/lambda/lambda_function_payload.zip \
+--environment "Variables={ENVIRONMENT=${ENVIRONMENT}, DEBUG=${DEBUG}, LAMBDA_DEBUG_MODE=${LAMBDA_DEBUG_MODE}, \
+    AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}, AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}, \
+    PYDEVD_DISABLE_FILE_VALIDATION=${PYDEVD_DISABLE_FILE_VALIDATION}}" \
 --handler main.lambda_handler --runtime python3.12 \
 --role arn:aws:iam::000000000000:role/lambda-role 
 
